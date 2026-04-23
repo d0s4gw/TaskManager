640816c20a40cfd859074e00e512cea70535822a
Michael Welsh <michael.j.welsh@gmail.com>
2026-04-23 12:01:14 -0700

    refactor: update OpenTelemetry resource configuration to use Resource class and ATTR_SERVICE_NAME

--- server/src/tracing.ts
+++ server/src/tracing.ts
@@ -1,12 +1,12 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { Resource } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 const sdk = new NodeSDK({
-  resource: resourceFromAttributes({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'task-manager-api',
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: 'task-manager-api',
   }),
   traceExporter: new OTLPTraceExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
