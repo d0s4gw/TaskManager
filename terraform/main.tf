49a23f9ca8542826fb831a198502d852be4a4c2c
Michael Welsh <michael.j.welsh@gmail.com>
2026-04-23 10:50:10 -0700

    fix: adjust build context and enable log streaming

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,7 +115,8 @@ resource "google_project_iam_member" "deployer_roles" {
     "roles/iam.workloadIdentityPoolAdmin", # To manage WIF
     "roles/resourcemanager.projectIamAdmin", # To manage Project IAM
     "roles/iam.serviceAccountAdmin", # To manage Service Account IAM
-    "roles/cloudbuild.builds.editor" # To check build status
+    "roles/cloudbuild.builds.editor", # To check build status
+    "roles/logging.viewer" # To stream build logs
   ])
   project = var.project_id
   role    = each.key
